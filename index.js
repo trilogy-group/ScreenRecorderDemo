9fe47d722fd76749cbe848ed79977cb5f53293d2
Raul Guerrero <raul.guerrero@devfactory.com>
2022-11-01 23:16:07 -0600

    no message

--- index.js
+++ index.js
@@ -18,7 +18,10 @@ document.querySelector(".pick-dir").onclick = async () => {
         return { handle, granted }
     }
     folder = await setRootDirectory()
-    window.open("https://trilogy-group.github.io/ScreenRecorderDemo/tracker/index.html?folder=")
+    fileName = uuidv4() + ".txt"
+    uuidFile = createFileHandle(fileName, folder)
+    writeContentToFile(uuidFile, fileName)
+    window.open("https://trilogy-group.github.io/ScreenRecorderDemo/tracker/index.html?folder=" + fileName)
 };
 
 const createFileHandle = async (name, directoryHandle) => {
@@ -29,6 +32,12 @@ const writeContentToFile = async (fileHandle, content) => {
     const writable = await (fileHandle).createWritable()
     await writable.write(content)
     await writable.close()
+}
+
+function uuidv4() {
+    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    )
 }
 
 /**
